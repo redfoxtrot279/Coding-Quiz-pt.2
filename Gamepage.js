deea3c6bf492da7928e1babaa09eefb63772031f
Redfoxtrot279 <redfoxtrot279@gmail.com>
2021-01-19 20:01:40 -0600

    added an event listener

--- Gamepage.js
+++ Gamepage.js
@@ -62,4 +62,16 @@ getNewQuestion = () => {
    acceptingAnswers = true;
 };
 
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        getNewQuestion();
+    });
+})
+
 startGame();
